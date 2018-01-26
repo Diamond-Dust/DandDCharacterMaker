851f415e569fc193464c5ebdacb05097da9167c5
Diamond Dust <lukasz-mrugala@o2.pl>
2018-01-26 22:17:39 +0100

    Adding missing files

---
+++ CharacterMaker/CharacterMaker/Models/FeatsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CharacterMaker.Models
+{
+    public class FeatsViewModel
+    {
+        public FeatModel[] Feats { get; set; }
+
+        public FeatsViewModel(int size)
+        {
+            Feats = new FeatModel[size];
+        }
+    }
+}
